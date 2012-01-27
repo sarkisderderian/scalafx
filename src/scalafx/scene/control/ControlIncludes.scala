b3575420f2238f487d69f2705db2089cebe6b7fe
rafael.afonso <rafael.afonso@localhost>
2012-01-26 22:32:45 -0200

    Added CheckBox.

---
+++ src/scalafx/scene/control/ControlIncludes.scala
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scalafx.scene.control
+import javafx.scene.{control => jfxsc}
+
+object ControlIncludes extends ControlIncludes
+trait ControlIncludes {
+  implicit def jfxAccordion2sfx(a: jfxsc.Accordion) = new Accordion(a)
+  implicit def jfxToggleGroup2sfx(tg: jfxsc.ToggleGroup) = new ToggleGroup(tg)
+  implicit def jfxToggleButton2sfx(tb: jfxsc.ToggleButton) = new ToggleButton(tb)
+  implicit def jfxRadioButton2sfx(rb: jfxsc.RadioButton) = new RadioButton(rb)
+  implicit def jfxButton2sfx(b: jfxsc.Button) = new Button(b)
+  implicit def jfxChoiceBox2sfx[J <: AnyRef](cb: jfxsc.ChoiceBox[J]) = new ChoiceBox[J](cb)
+  implicit def jfxCheckBox2sfx(c: jfxsc.CheckBox) = new CheckBox(c)
+  implicit def jfxControl2sfx(c: jfxsc.Control) = new Control(c) {}
+  implicit def jfxLabel2sfx(l: jfxsc.Label) = new Label(l)
+  implicit def jfxLabeled2sfx(l: jfxsc.Labeled) = new Labeled(l) {}
+  implicit def jfxScrollPane2sfx(s: jfxsc.ScrollPane) = new ScrollPane(s) {}
+  implicit def jfxTitledPane2sfx(t: jfxsc.TitledPane) = new TitledPane(t)
+}
