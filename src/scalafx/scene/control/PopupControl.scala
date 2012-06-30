7f9859c5b844ecc1c169f5fb04e3a523b2507424
rafael.afonso <rafael.afonso@localhost>
2012-06-30 22:00:02 +1000

    Changes to amend with previous

---
+++ src/scalafx/scene/control/PopupControl.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2012, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.control
+
+import javafx.scene.{ control => jfxsc }
+import scalafx.Includes._
+import scalafx.stage.PopupWindow
+import scalafx.util.SFXDelegate
+
+object PopupControl {
+  implicit def sfxPopupControl2jfx(v: PopupControl) = v.delegate
+}
+
+class PopupControl(override val delegate: jfxsc.PopupControl = new jfxsc.PopupControl)
+  extends PopupWindow(delegate)
+  with Skinnable
+  with SFXDelegate[jfxsc.PopupControl] {
+
+  /**
+   * The id of this Node.
+   */
+  def id = delegate.idProperty
+  def id_=(v: String) {
+    id = v
+  }
+
+  /**
+   * Property for overriding the control's computed maximum height.
+   */
+  def maxHeight = delegate.maxHeightProperty
+  def maxHeight_=(v: Double) {
+    maxHeight = v
+  }
+
+  /**
+   * Property for overriding the control's computed maximum width.
+   */
+  def maxWidth = delegate.maxWidthProperty
+  def maxWidth_=(v: Double) {
+    maxWidth = v
+  }
+
+  /**
+   * Property for overriding the control's computed minimum height.
+   */
+  def minHeight = delegate.minHeightProperty
+  def minHeight_=(v: Double) {
+    minHeight = v
+  }
+
+  /**
+   * Property for overriding the control's computed minimum width.
+   */
+  def minWidth = delegate.minWidthProperty
+  def minWidth_=(v: Double) {
+    minWidth = v
+  }
+
+  /**
+   * Property for overriding the control's computed preferred height.
+   *
+   */
+  def prefHeight = delegate.prefHeightProperty
+  def prefHeight_=(v: Double) {
+    prefHeight = v
+  }
+
+  /**
+   * Property for overriding the control's computed preferred width.
+   */
+  def prefWidth = delegate.prefWidthProperty
+  def prefWidth_=(v: Double) {
+    prefWidth = v
+  }
+
+  /**
+   * A string representation of the CSS style associated with this specific Node.
+   */
+  def style = delegate.styleProperty
+  def style_=(v: String) {
+    style = v
+  }
+
+  def styleClass = delegate.getStyleClass
+
+}
---
+++ src/scalafx/scene/shape/ArcTo.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package scalafx.scene.shape
+
+import javafx.scene.{ shape => jfxss }
+import scalafx.Includes._
+import scalafx.util.PositionDelegate
+import scalafx.util.SFXDelegate
+
+object ArcTo {
+  implicit def sfxArcTo2jfx(v: ArcTo) = v.delegate
+
+  def apply(radiusX: Double, radiusY: Double, xAxisRotation: Double, x: Double, y: Double, largeArcFlag: Boolean, sweepFlag: Boolean) =
+    new ArcTo(new jfxss.ArcTo(radiusX, radiusY, xAxisRotation, x, y, largeArcFlag, sweepFlag))
+}
+
+class ArcTo(override val delegate: jfxss.ArcTo = new jfxss.ArcTo)
+  extends PathElement(delegate)
+  with PositionDelegate
+  with SFXDelegate[jfxss.ArcTo] {
+
+  def positionedDelegate = delegate.asInstanceOf[Positioned]
+
+  def XAxisRotation = delegate.XAxisRotationProperty
+  def XAxisRotation_=(v: Double) {
+    XAxisRotation() = v
+  }
+
+  def sweepFlag = delegate.sweepFlagProperty
+  def sweepFlag_=(v: Boolean) {
+    sweepFlag() = v
+  }
+
+  def radiusY = delegate.radiusYProperty
+  def radiusY_=(v: Double) {
+    radiusY() = v
+  }
+
+  def radiusX = delegate.radiusXProperty
+  def radiusX_=(v: Double) {
+    radiusX() = v
+  }
+
+  def largeArcFlag = delegate.largeArcFlagProperty
+  def largeArcFlag_=(v: Boolean) {
+    largeArcFlag() = v
+  }
+
+}
