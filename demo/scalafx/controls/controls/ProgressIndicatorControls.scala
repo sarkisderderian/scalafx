8f2916ac640ff7d255831cddea99c91d80573def
Peter Pilgrim <peter.pilgrim@gmail.com>
2012-08-27 11:16:14 +0100

    Corrected Gradle project group and version

---
+++ demo/scalafx/controls/controls/ProgressIndicatorControls.scala
@@ -0,0 +1,26 @@
+package scalafx.controls.controls
+
+import scalafx.Includes.eventClosureWrapper
+import scalafx.Includes._
+import scalafx.scene.control.Label
+import scalafx.scene.control.ProgressIndicator
+import scalafx.scene.control.TextField
+
+class ProgressIndicatorControls(target: ProgressIndicator) extends PropertiesNodes[ProgressIndicator](target, target.getClass().getSimpleName() + " Properties") {
+
+  val txfValue = new TextField 
+  txfValue.onAction = fillDoublePropertyFromText(target.progress, txfValue, true, () => (target.progress = ProgressIndicator.INDETERMINATE_PROGRESS)) 
+
+  val lblRealValue = new Label {
+    text <== target.progress.asString()
+  }
+
+  val lblIndeterminate = new Label {
+    text <== target.indeterminate.asString()
+  }
+
+  super.addNode("Value", txfValue)
+  super.addNode("Real Value", lblRealValue)
+  super.addNode("Indeterminate", lblIndeterminate)
+
+}
