9dd475a54aae69e62c6736f3ce0cc8f26962d60a
Jarek Sacha <jpsacha@gmail.com>
2013-02-20 21:12:37 -0500

    Prepare for ScalaFX 1.0.0-M1 release - create 'stable' branch for releases and update version numbers.

---
+++ scalafx/src/main/scala/scalafx/animation/StrokeTransition.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.animation
+
+import javafx.util.Duration
+import javafx.{ animation => jfxa }
+import scalafx.Includes._
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Shape
+import scalafx.delegate.SFXDelegate
+
+object StrokeTransition extends AnimationStatics {
+  implicit def sfxStrokeTransition2jfx(v: StrokeTransition) = v.delegate
+}
+
+/**
+ * Wraps a [[http://docs.oracle.com/javafx/2/api/javafx/animation/StrokeTransition.html StrokeTransition]].
+ */
+class StrokeTransition(override val delegate: jfxa.StrokeTransition = new jfxa.StrokeTransition)
+  extends Transition(delegate)
+  with SFXDelegate[jfxa.StrokeTransition] {
+
+  /**
+   * The constructor of StrokeTransition
+   *
+   * @param duration The duration of the StrokeTransition
+   * @param shape The shape which filling will be animated
+   * @param fromValue The start value of the color-animation
+   * @param toValue The end value of the color-animation
+   */
+  def this(duration: Duration, shape: Shape, fromValue: Color, toValue: Color) =
+    this(new jfxa.StrokeTransition(duration, shape, fromValue, toValue))
+
+  /**
+   * The constructor of StrokeTransition
+   *
+   * @param duration The duration of the StrokeTransition
+   * @param fromValue The start value of the color-animation
+   * @param toValue The end value of the color-animation
+   */
+  def this(duration: Duration, fromValue: Color, toValue: Color) =
+    this(new jfxa.StrokeTransition(duration, fromValue, toValue))
+
+  /**
+   * The constructor of StrokeTransition
+   *
+   * @param duration The duration of the StrokeTransition
+   * @param shape The shape which filling will be animated
+   */
+  def this(duration: Duration, shape: Shape) =
+    this(new jfxa.StrokeTransition(duration, shape))
+
+  /**
+   * The constructor of StrokeTransition
+   *
+   * @param duration The duration of the StrokeTransition
+   */
+  def this(duration: Duration) = this(new jfxa.StrokeTransition(duration))
+
+  /**
+   * The target shape of this StrokeTransition.
+   */
+  def shape = delegate.shapeProperty
+  def shape_=(s: Shape) {
+    shape() = s
+  }
+
+  /**
+   * The duration of this StrokeTransition.
+   */
+  def duration = delegate.durationProperty
+  def duration_=(d: Duration) {
+    duration() = d
+  }
+
+  /**
+   * Specifies the start color value for this StrokeTransition.
+   */
+  def fromValue = delegate.fromValueProperty
+  def fromValue_=(from: Color) {
+    fromValue() = from
+  }
+
+  /**
+   * Specifies the stop color value for this StrokeTransition.
+   */
+  def toValue = delegate.toValueProperty
+  def toValue_=(to: Color) {
+    toValue() = to
+  }
+}
---
+++ scalafx/src/main/scala/scalafx/scene/control/TreeView.scala
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2011, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.scene.control
+
+import javafx.scene.{control => jfxsc}
+import javafx.{event => jfxe}
+import javafx.{util => jfxu}
+import scalafx.Includes._
+import scalafx.event.Event
+import scalafx.delegate.SFXDelegate
+
+object TreeView {
+  implicit def sfxTreeView2jfx[T](v: TreeView[T]) = v.delegate
+
+  object EditEvent {
+    implicit def sfxTreeViewEditEvent2jfx[T](v: EditEvent[T]) = v.delegate
+  }
+  
+  class EditEvent[T](override val delegate: jfxsc.TreeView.EditEvent[T])
+    extends Event(delegate)
+    with SFXDelegate[jfxsc.TreeView.EditEvent[T]] {
+
+    /**
+     * Creates a new EditEvent instance to represent an edit event.
+     */
+    def this(source: TreeView[T], eventType: jfxe.EventType[_ <: jfxsc.TreeView.EditEvent[T]], treeItem: TreeItem[T], oldValue: T, newValue: T) =
+      this(new jfxsc.TreeView.EditEvent[T](source, eventType, treeItem, oldValue, newValue))
+
+    /**
+     * Returns the new value input into the TreeItem by the end user.
+     */
+    def newValue = delegate.getNewValue
+
+    /**
+     * Returns the old value that existed in the TreeItem prior to the current edit event.
+     */
+    def oldValue = delegate.getOldValue
+
+    /**
+     * Returns the TreeView upon which the edit took place.
+     */
+    override def source = delegate.getSource
+
+    /**
+     * Returns the `TreeItem` upon which the edit took place.
+     */
+    def treeItem = delegate.getTreeItem
+
+  }
+
+  /**
+   * An EventType that indicates some edit event has occurred.
+   */
+  def editAnyEvent = jfxsc.TreeView.editAnyEvent
+
+  /**
+   * An EventType used to indicate that an edit event has just been canceled
+   * within the TreeView upon which the event was fired.
+   */
+  def editCancelEvent = jfxsc.TreeView.editCancelEvent
+
+  /**
+   * An EventType that is used to indicate that an edit in a TreeView has been
+   *  committed.
+   */
+  def editCommitEvent = jfxsc.TreeView.editCommitEvent
+
+  /**
+   * An EventType used to indicate that an edit event has started within the
+   * TreeView upon which the event was fired.
+   */
+  def editStartEvent = jfxsc.TreeView.editStartEvent
+
+  /**
+   * Returns the number of levels of 'indentation' of the given TreeItem,
+   * based on how many times getParent() can be recursively called.
+   */
+  def nodeLevel(node: TreeItem[_]) = jfxsc.TreeView.getNodeLevel(node)
+
+  /**
+   * Creates a new TreeView overriding layoutChildren method from JavaFX`s
+   * TreeView.
+   */
+  def apply[T](layoutChildren: => Unit) = new TreeView[T](new jfxsc.TreeView[T] {
+    override def layoutChildren = layoutChildren
+  })
+
+}
+
+class TreeView[T](override val delegate: jfxsc.TreeView[T] = new jfxsc.TreeView[T])
+  extends Control(delegate)
+  with SFXDelegate[jfxsc.TreeView[T]] {
+
+  /**
+   * Creates a TreeView with the provided root node.
+   */
+  def this(rootItem: TreeItem[T]) = this(new jfxsc.TreeView[T](rootItem))
+
+  def cellFactory = delegate.cellFactoryProperty
+  def cellFactory_=(v: (TreeView[T] => jfxsc.TreeCell[T])) {
+    cellFactory() = new jfxu.Callback[jfxsc.TreeView[T], jfxsc.TreeCell[T]] {
+      def call(tv: jfxsc.TreeView[T]): jfxsc.TreeCell[T] = {
+        v(tv)
+      }
+    }
+  }
+
+  /**
+   * Specifies whether this TreeView is editable - only if the TreeView and
+   * the TreeCells within it are both editable will a TreeCell be able to go
+   * into their editing state.
+   */
+  def editable = delegate.editableProperty
+  def editable_=(v: Boolean) {
+    editable() = v
+  }
+
+  /**
+   * A property used to represent the TreeItem currently being edited in the
+   * TreeView, if editing is taking place, or -1 if no item is being edited.
+   */
+  def editingItem = delegate.editingItemProperty
+
+  /**
+   * The FocusModel provides the API through which it is possible to control
+   * focus on zero or one rows of the TreeView.
+   */
+  def focusModel = delegate.focusModelProperty
+  def focusModel_=(v: jfxsc.FocusModel[jfxsc.TreeItem[T]]) {
+    focusModel() = v
+  }
+
+  /**
+   * This event handler will be fired when the user cancels editing a cell.
+   */
+  def onEditCancel = delegate.onEditCancelProperty
+  def onEditCancel_=(v: jfxe.EventHandler[jfxsc.TreeView.EditEvent[T]]) {
+    onEditCancel() = v
+  }
+
+  /**
+   * This event handler will be fired when the user commits editing a cell.
+   */
+  def onEditCommit = delegate.onEditCommitProperty
+  def onEditCommit_=(v: jfxe.EventHandler[jfxsc.TreeView.EditEvent[T]]) {
+    onEditCommit() = v
+  }
+
+  /**
+   * This event handler will be fired when the user starts editing a cell.
+   */
+  def onEditStart = delegate.onEditStartProperty
+  def onEditStart_=(v: jfxe.EventHandler[jfxsc.TreeView.EditEvent[T]]) {
+    onEditStart() = v
+  }
+
+  /**
+   * Property representing the root node of the TreeView.
+   */
+  def root = delegate.rootProperty
+  def root_=(v: TreeItem[T]) {
+    root() = v
+  }
+
+  /**
+   *
+   */
+  def selectionModel = delegate.selectionModelProperty
+  def selectionModel_=(v: jfxsc.MultipleSelectionModel[jfxsc.TreeItem[T]]) {
+    selectionModel() = v
+  }
+
+  /**
+   * Property that represents whether or not the TreeView root node is visible.
+   */
+  def showRoot = delegate.showRootProperty
+  def showRoot_=(v: Boolean) {
+    showRoot() = v
+  }
+
+  /**
+   * Instructs the TreeView to begin editing the given TreeItem, if the
+   * TreeView is `editable`.
+   */
+  def edit(item: TreeItem[T]) = delegate.edit(item)
+
+  /**
+   * Returns the index position of the given TreeItem, taking into account the
+   * current state of each TreeItem (i.e. whether or not it is expanded).
+   */
+  def row(item: TreeItem[T]) = delegate.getRow(item)
+
+}
