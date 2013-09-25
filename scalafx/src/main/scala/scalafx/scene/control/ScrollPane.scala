7960cbecbfa9ee8317a127c166428039101f9fa4
Mike Allen <mike@hindsight-consulting.com>
2013-09-25 11:12:35 -0400

    Automatically configure JVM options for executing SBT if using the "sbt-extras" SBT running (available from https://github.com/paulp/sbt-extras).

---
+++ scalafx/src/main/scala/scalafx/scene/control/ScrollPane.scala
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2011-2013, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.scene.control
+
+import javafx.scene.{ control => jfxsc }
+import javafx.{ geometry => jfxg }
+import scalafx.Includes._
+import scalafx.geometry.Bounds
+import scalafx.delegate.SFXDelegate
+import scalafx.beans.property._
+import scalafx.delegate.{ SFXEnumDelegateCompanion, SFXEnumDelegate }
+import javafx.{ scene => jfxs }
+import scalafx.scene.Node
+
+object ScrollPane {
+  implicit def sfxScrollPane2jfx(v: ScrollPane) = v.delegate
+
+  object ScrollBarPolicy
+    extends SFXEnumDelegateCompanion[jfxsc.ScrollPane.ScrollBarPolicy, ScrollBarPolicy] {
+
+    /** Indicates that a scroll bar should always be shown. */
+    val ALWAYS = new ScrollBarPolicy(jfxsc.ScrollPane.ScrollBarPolicy.ALWAYS)
+
+    /** Indicates that a scroll bar should be shown when required. */
+    val AS_NEEDED = new ScrollBarPolicy(jfxsc.ScrollPane.ScrollBarPolicy.AS_NEEDED)
+
+    /** Indicates that a scroll bar should never be shown */
+    val NEVER = new ScrollBarPolicy(jfxsc.ScrollPane.ScrollBarPolicy.NEVER)
+
+    protected override def unsortedValues: Array[ScrollBarPolicy] = Array(ALWAYS, AS_NEEDED, NEVER)
+
+  }
+
+  /** Wrapper for [[http://docs.oracle.com/javafx/2/api/javafx/scene/control/ScrollPane.ScrollBarPolicy.html]] */
+  sealed case class ScrollBarPolicy(override val delegate: jfxsc.ScrollPane.ScrollBarPolicy)
+    extends SFXEnumDelegate[jfxsc.ScrollPane.ScrollBarPolicy]
+
+}
+
+class ScrollPane(override val delegate: jfxsc.ScrollPane = new jfxsc.ScrollPane)
+  extends Control(delegate)
+  with SFXDelegate[jfxsc.ScrollPane] {
+
+  /**
+   * The node used as the content of this ScrollPane.
+   */
+  def content: ObjectProperty[jfxs.Node] = delegate.contentProperty
+
+  @deprecated("Use a ScalaFX Node instead a JavaFX Node", "1.0")
+  def content_=(v: jfxs.Node) {
+    content() = v
+  }
+
+  def content_=(v: Node) {
+    content() = v
+  }
+
+  /**
+   * If true and if the contained node is a Resizable, then the node will be kept resized to match the height of the 
+   * ScrollPane's viewport.
+   */
+  def fitToHeight: BooleanProperty = delegate.fitToHeightProperty
+  def fitToHeight_=(v: Boolean) {
+    fitToHeight() = v
+  }
+
+  /**
+   * If true and if the contained node is a Resizable, then the node will be kept resized to match the width of the 
+   * ScrollPane's viewport.
+   */
+  def fitToWidth: BooleanProperty = delegate.fitToWidthProperty
+  def fitToWidth_=(v: Boolean) {
+    fitToWidth() = v
+  }
+
+  /**
+   * Specifies the policy for showing the horizontal scroll bar.
+   */
+  def hbarPolicy: ObjectProperty[jfxsc.ScrollPane.ScrollBarPolicy] = delegate.hbarPolicyProperty
+  def hbarPolicy_=(v: ScrollPane.ScrollBarPolicy) {
+    hbarPolicy() = v
+  }
+
+  /**
+   * The maximum allowable hvalue for this ScrollPane.
+   */
+  def hmax: DoubleProperty = delegate.hmaxProperty
+  def hmax_=(v: Double) {
+    hmax() = v
+  }
+
+  /**
+   * The minimum allowable hvalue for this ScrollPane.
+   */
+  def hmin: DoubleProperty = delegate.hminProperty
+  def hmin_=(v: Double) {
+    hmin() = v
+  }
+
+  /**
+   * The current horizontal scroll position of the ScrollPane.
+   */
+  def hvalue: DoubleProperty = delegate.hvalueProperty
+  def hvalue_=(v: Double) {
+    hvalue() = v
+  }
+
+  /**
+   * Specifies whether the user should be able to pan the viewport by using the mouse.
+   */
+  def pannable: BooleanProperty = delegate.pannableProperty
+  def pannable_=(v: Boolean) {
+    pannable() = v
+  }
+
+  /**
+   * Specify the preferred height of the ScrollPane Viewport.
+   */
+  def prefViewportHeight: DoubleProperty = delegate.prefViewportHeightProperty
+  def prefViewportHeight_=(v: Double) {
+    prefViewportHeight() = v
+  }
+
+  /**
+   * Specify the preferred width of the ScrollPane Viewport.
+   */
+  def prefViewportWidth: DoubleProperty = delegate.prefViewportWidthProperty
+  def prefViewportWidth_=(v: Double) {
+    prefViewportWidth() = v
+  }
+
+  /**
+   * Specifies the policy for showing the vertical scroll bar.
+   */
+  def vbarPolicy: ObjectProperty[jfxsc.ScrollPane.ScrollBarPolicy] = delegate.vbarPolicyProperty
+  def vbarPolicy_=(v: ScrollPane.ScrollBarPolicy) {
+    vbarPolicy() = v
+  }
+
+  /**
+   * The actual Bounds of the ScrollPane Viewport.
+   */
+  def viewportBounds: ObjectProperty[jfxg.Bounds] = delegate.viewportBoundsProperty
+  def viewportBounds_=(v: Bounds) {
+    viewportBounds() = v
+  }
+
+  /**
+   * The maximum allowable vvalue for this ScrollPane.
+   */
+  def vmax: DoubleProperty = delegate.vmaxProperty
+  def vmax_=(v: Double) {
+    vmax() = v
+  }
+
+  /**
+   * The minimum allowable vvalue for this ScrollPane.
+   */
+  def vmin: DoubleProperty = delegate.vminProperty
+  def vmin_=(v: Double) {
+    vmin() = v
+  }
+
+  /**
+   * The current vertical scroll position of the ScrollPane.
+   */
+  def vvalue: DoubleProperty = delegate.vvalueProperty
+  def vvalue_=(v: Double) {
+    vvalue() = v
+  }
+
+}
---
+++ scalafx/src/main/scala/scalafx/scene/web/WebEngine.scala
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2011-2013, ScalaFX Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scalafx.scene.web
+
+import javafx.{ event => jfxe }
+import javafx.scene.{ web => jfxsw }
+import javafx.{ util => jfxu }
+import scalafx.Includes._
+import scalafx.beans.property._
+import javafx.geometry.Rectangle2D
+import scalafx.delegate.SFXDelegate
+import org.w3c.dom.Document
+
+object WebEngine {
+  implicit def sfxWebEngine2jfx(we: WebEngine) = we.delegate
+}
+
+class WebEngine(override val delegate: jfxsw.WebEngine = new jfxsw.WebEngine) extends SFXDelegate[jfxsw.WebEngine] {
+
+  /**
+   * Creates a new engine and loads a Web page into it.
+   */
+  def this(url: String) = this(new jfxsw.WebEngine(url))
+
+  /**
+   * JavaScript confirm handler property.
+   */
+  def confirmHandler = delegate.confirmHandlerProperty
+  def confirmHandler_=(f: String => Boolean) {
+    confirmHandler() = new jfxu.Callback[java.lang.String, java.lang.Boolean] {
+      def call(msg: java.lang.String) = f(msg)
+    }
+  }
+
+  /**
+   * JavaScript popup handler property.
+   */
+  def createPopupHandler = delegate.createPopupHandlerProperty
+  def createPopupHandler_=(f: jfxsw.PopupFeatures => WebEngine) {
+    createPopupHandler() = new jfxu.Callback[jfxsw.PopupFeatures, jfxsw.WebEngine] {
+      def call(pf: jfxsw.PopupFeatures) = f(pf)
+    }
+  }
+
+  /**
+   * Document object for the current Web page.
+   */
+  def document: Document = delegate.getDocument
+
+  /**
+   * URL of the current Web page.
+   */
+  def location: ReadOnlyStringProperty = delegate.locationProperty
+
+  /**
+   * JavaScript alert handler property.
+   */
+  def onAlert = delegate.onAlertProperty
+  def onAlert_=(v: jfxe.EventHandler[jfxsw.WebEvent[String]]) {
+    onAlert() = v
+  }
+
+  /** The event handler called when an error occurs. */
+  def onError = delegate.onErrorProperty
+  def onError_=(v: jfxe.EventHandler[jfxsw.WebErrorEvent]) {
+    onAlert() = v
+  }
+
+  /**
+   * JavaScript window resize handler property.
+   */
+  def onResized = delegate.onResizedProperty
+  def onResized_=(v: jfxe.EventHandler[jfxsw.WebEvent[Rectangle2D]]) {
+    onResized() = v
+  }
+
+  /**
+   * JavaScript status handler property.
+   */
+  def onStatusChanged = delegate.onStatusChangedProperty
+  def onStatusChanged_=(v: jfxe.EventHandler[jfxsw.WebEvent[String]]) {
+    onStatusChanged() = v
+  }
+
+  /**
+   * JavaScript window visibility handler property.
+   */
+  def onVisibilityChanged = delegate.onVisibilityChangedProperty
+  def onVisibilityChanged_=(v: jfxe.EventHandler[jfxsw.WebEvent[java.lang.Boolean]]) {
+    onVisibilityChanged() = v
+  }
+
+  /**
+   * JavaScript prompt handler property.
+   */
+  def promptHandler = delegate.promptHandlerProperty
+  def promptHandler_=(f: PromptData => String) {
+    promptHandler() = new jfxu.Callback[jfxsw.PromptData, String] {
+      def call(pd: jfxsw.PromptData) = f(pd)
+    }
+  }
+
+  /**
+   * JavaScript enabled handler property.
+   *
+   * @since 2.2
+   */
+  def javaScriptEnabled: BooleanProperty = delegate.javaScriptEnabledProperty
+  def javaScriptEnabled_=(v: Boolean) {
+    javaScriptEnabled() = v
+  }
+
+  /** Specifies the directory to be used by this WebEngine to store local user data. */
+  def userDataDirectory : ObjectProperty[java.io.File] = delegate.userDataDirectoryProperty
+  def userDataDirectory_=(v:java.io.File) {
+    ObjectProperty.fillProperty[java.io.File](this.userDataDirectory, v)
+  }
+
+  /**
+   * JavaScript enabled handler property.
+   *
+   * @since 2.2
+   */
+  def userStyleSheetLocation: StringProperty = delegate.userStyleSheetLocationProperty
+  def userStyleSheetLocation_=(v: String) {
+    userStyleSheetLocation() = v
+  }
+
+  /**
+   * Title of the current Web page.
+   */
+  def title: ReadOnlyStringProperty = delegate.titleProperty
+
+  /**
+   * Loads a Web page into this engine.
+   */
+  def load(url: String) {
+    delegate.load(url)
+  }
+
+  /**
+   * Loads the given HTML content directly.
+   */
+  def loadContent(content: String) {
+    delegate.loadContent(content)
+  }
+
+  /**
+   * Loads the given content directly.
+   */
+  def loadContent(content: String, contentType: String) {
+    delegate.loadContent(content, contentType)
+  }
+
+  def userAgent : StringProperty = delegate.userAgentProperty
+  def userAgent_=(v:String) {
+    userAgent() = v
+  }
+}
